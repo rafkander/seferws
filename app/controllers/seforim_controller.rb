7effe4e414ee9c4cbe22ce04351df238e2393cfa
rafkander <150948247+rafkander@users.noreply.github.com>
2024-07-18 19:16:03 +0100

    changed the pages names from index to new

--- app/controllers/seforim_controller.rb
+++ app/controllers/seforim_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/listingform_controller.rb
-class ListingformController < ApplicationController
-    def index
+class SeforimController < ApplicationController
+    def new
       # Any logic for the index action can go here
     end
   end
