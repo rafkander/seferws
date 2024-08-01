e97e2e98e894e7f59b908ffd1763c3dde2e645ae
rafkander <150948247+rafkander@users.noreply.github.com>
2024-08-01 22:12:54 +0100

    code we did in lesson made it link to db

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,26 @@
 # app/controllers/listingform_controller.rb
 class ListingsController < ApplicationController
-    def new
-      @listing = Listing.new
-      # Any logic for the index action can go here
-    end
+  def new
+    @listing = Listing.new
+    # Any logic for the index action can go here
+  end
 
-    def create
-      puts "hey you got to the create method"
-   
-
+  def create
+    @listing = Listing.new(listing_params)
+    
+    if @listing.save
+      redirect_to root_path, notice: 'Sefer was successfully listed.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(
+      :sefer_name, :category_id, :edition, :sefer_language,
+      :set_details, :name, :sefer_location, :sefer_condition, :sefer_description, :cover
+    )
+  end
+end
